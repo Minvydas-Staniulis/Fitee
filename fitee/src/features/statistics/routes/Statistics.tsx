9fa865c2159e077b9ec6fc886bca4da85190e495
Minvydas-Staniulis <mimciks@gmail.com>
2024-04-05 17:02:34 +0300

    totaldistance

--- fitee/src/features/statistics/routes/Statistics.tsx
+++ fitee/src/features/statistics/routes/Statistics.tsx
@@ -1,3 +1,27 @@
+import axios from "axios";
+import React from "react";
+
 export default function Workouts() {
-  return <></>;
+  const [totalDistance, setTotalDistance] = React.useState<Number>(0);
+
+  const fetchDistance = () => {
+    axios
+      .get(`http://localhost:5015/api/RunningApi/TotalRunDistance`)
+      .then((response) => {
+        setTotalDistance(response.data.totalDistance);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchDistance();
+  });
+
+  return (
+    <div>
+      <p>Total distance: {totalDistance.toString()}</p>
+    </div>
+  );
 }
