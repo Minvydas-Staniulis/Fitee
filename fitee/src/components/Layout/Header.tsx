a92491a1e8317f2cd810a69145f5972cb6a6a98f
Minvydas-Staniulis <mimciks@gmail.com>
2024-04-22 00:18:08 +0300

    responsive header

--- fitee/src/components/Layout/Header.tsx
+++ fitee/src/components/Layout/Header.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import Hidden from "@mui/material/Hidden";
 
 const navItems = [
   { label: "Home", path: "/app/home" },
@@ -13,24 +15,57 @@ const navItems = [
 ];
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
-          <Box>
+          <Hidden smDown>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item.label}
+                  color="inherit"
+                  component={Link}
+                  to={item.path}
+                >
+                  {item.label}
+                </Button>
+              ))}
+            </Box>
+          </Hidden>
+          <Hidden mdUp>
+            <Button color="inherit" onClick={toggleMenu}>
+              Menu
+            </Button>
+          </Hidden>
+        </Toolbar>
+        {isMenuOpen && (
+          <Box
+            p={2}
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-start"
+          >
             {navItems.map((item) => (
               <Button
                 key={item.label}
                 color="inherit"
                 component={Link}
                 to={item.path}
+                onClick={() => setIsMenuOpen(false)}
               >
                 {item.label}
               </Button>
             ))}
           </Box>
-        </Toolbar>
+        )}
       </AppBar>
     </Box>
   );
